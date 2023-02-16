20f75b64059eda592e1d11a300d94cf772371d39
Yaqrut <vim.in.swe@gmail.com>
2023-02-16 13:42:37 -0800

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by YAQRUT on 2023-02-16.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
